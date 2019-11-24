433bb8cf5884dce07bb760d46c093a173a98e234
Ivanilson Pereira Mota <ivanilsonmota@gmail.com>
2019-11-24 14:53:28 -0300

    API usuarios concluida!

---
+++ requeriments.php
@@ -0,0 +1,45 @@
+<?php
+
+|||||||||||||||||||||||||||||
+| Uso da API Usuários: |
+|||||||||||||||||||||||||||||
+
+
+Listar todos usuários:
+GET /api/v1/usuarios
+
+Localizar usuário pelo ID:
+GET /api/v1/usuarios/{id}
+
+Criar usuário:
+POST /api/v1/usuarios
+
+Exemplo de uso:
+
+{
+    "data": [
+        {
+            "first_name": "Usuário",
+            "last_name": "Sobrenome",
+            "email": "email@gmail.com",
+            "pwd": "senha"
+        }
+    ]
+}
+
+Atualizar usuário:
+PUT /api/v1/usuarios/{id}
+
+{
+    "data": [
+        {
+            "first_name": "Usuário 2",
+            "last_name": "Sobrenome 2",
+            "email": "usuario@gmail.com",
+            "pwd": "senha 2"
+        }
+    ]
+}
+
+Excluir usuário:
+DELETE /api/v1/usuarios/{id}
--- source/App/UserController.php
+++ source/App/UserController.php
@@ -10,6 +10,9 @@ class UserController
     public function __construct()
     {
         $this->users = new User();
+
+        header('Cache-Control: no-cache, must-revalidate');
+        header('Content-Type: application/json; charset=utf-8');
     }
 
     public function getAll(): void
@@ -30,10 +33,99 @@ class UserController
             }
 
             echo json_encode($users_arr);
-        }else{
-            echo json_encode(array(
-                "error_message" => "Não possui usuários cadastrados!"
-            ));
+        } else {
+            echo json_encode(array("error_message" => "Não possui usuários cadastrados!"));
         }
     }
+
+    public function getById($data)
+    {
+        $user = ($this->users)->findById($data["id"]);
+        if (($this->users)->findById($data["id"])) {
+            $user_arr["data"] = array();
+
+            $userItem = array(
+                "id"         => $user->id,
+                "first_name" => $user->first_name,
+                "last_name"  => $user->last_name,
+                "email"      => $user->email
+            );
+            array_push($user_arr["data"], $userItem);
+
+            echo json_encode($user_arr);
+        } else {
+            echo json_encode(array("error_message" => "O usuário informado não existe!"));
+        }
+    }
+
+    public function create(): void
+    {
+        $json = file_get_contents("php://input");
+        $json_content = json_decode($json);
+
+        foreach ($json_content as $usr) {
+            $user = $this->users;
+            $user->first_name = $usr[0]->first_name;
+            $user->last_name  = $usr[0]->last_name;
+            $user->email      = $usr[0]->email;
+            $user->pwd       =  md5($usr[0]->pwd);
+            $userId = $user->save();
+
+            if ($userId) {
+                echo json_encode(array("success" => "Usuário registrado com sucesso!"));
+            } else {
+                echo json_encode(array("error_message" => "Não foi possível cadastrar o usuário!"));
+            }
+        }
+    }
+
+    public function update($data): void
+    {
+        $json = file_get_contents("php://input");
+        $json_content = json_decode($json);
+
+        foreach ($json_content as $usr) {
+            $user = ($this->users)->findById($data["id"]);
+            $user->first_name = $usr[0]->first_name;
+            $user->last_name  = $usr[0]->last_name;
+            $user->email      = $usr[0]->email;
+            $user->pwd       =  md5($usr[0]->pwd);
+        }
+
+        $userId = $user->save();
+        if ($userId) {
+            echo json_encode(array("success" => "Usuário atualizado com sucesso!"));
+        } else {
+            echo json_encode(array("error_message" => "Não foi possível atualizar o usuário!"));
+        }
+    }
+
+
+    public function delete($data): void
+    {
+        $user = ($this->users)->findById($data["id"]);
+        if ($user->destroy()) {
+            echo json_encode(array("success" => "Usuário excluído com sucesso!"));
+        } else {
+            echo json_encode(array("error_message" => "Não foi possível excluir o usuário!"));
+        }
+    }
+
+    /*     public function getByEmail($data){
+        if (($this->users)->find($data["id"])->count() > 0) {
+            $user_arr["data"] = array();
+            $user = ($this->users)->find($data["id"])->fetch();
+            $userItem = array(
+                "id"         => $user->id,
+                "first_name" => $user->first_name,
+                "last_name"  => $user->last_name,
+                "email"      => $user->email
+            );
+            array_push($user_arr["data"], $userItem);
+
+            echo json_encode($user_arr);
+        } else {
+            echo json_encode(array("error_message" => "Não existe usuário cadastrado com o email {$data["email"]}"));
+        }
+    } */
 }
